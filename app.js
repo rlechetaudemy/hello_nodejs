200cf1e38bb51067b14643405cfd0f6d95f27a3a
rlecheta <rlecheta@livetouch.com.br>
2020-02-16 17:05:54 -0300

    start script

--- app.js
+++ app.js
@@ -2,6 +2,6 @@ var port = process.env.PORT || 3000;
 var http = require('http');
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello Node.JS 3!');
+  res.end('Hello Node.JS 4!');
 }).listen(port);
 console.log('Server running at http://localhost:8080/');
